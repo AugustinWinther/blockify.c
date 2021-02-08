cb0fe4d495e42d95bd31bedbc373dfeae55ddad8
Augustin Winther <augustin@winther.io>
2021-02-08 01:00:08 +0100

    style(main): Add comments

--- blockify.c
+++ blockify.c
@@ -7,24 +7,25 @@
     #define Sleep(x) usleep((x)*1000)
 #endif
 
-#define STB_IMAGE_IMPLEMENTATION
-#define STBI_MAX_DIMENSIONS 20000
-#define STBI_ONLY_PNG
-#include "include/stb_image.h"
-
-#define STB_IMAGE_RESIZE_IMPLEMENTATION
-#include "include/stb_image_resize.h"
-
-#define STB_IMAGE_WRITE_IMPLEMENTATION
-#include "include/stb_image_write.h"
-
+// Standard libraries
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
 #include <math.h>
 
+// Third party Libraries
+#define STB_IMAGE_IMPLEMENTATION
+#define STBI_MAX_DIMENSIONS 20000
+#define STBI_ONLY_PNG
+#include "include/stb_image.h"
+#define STB_IMAGE_RESIZE_IMPLEMENTATION
+#include "include/stb_image_resize.h"
+#define STB_IMAGE_WRITE_IMPLEMENTATION
+#include "include/stb_image_write.h"
+
 int main(int argc, char* argv[])  {
     
+    // Check if argument is passed
     if (argc < 2){
         printf("ERROR: No input image...\n");
         Sleep(3000);
@@ -41,6 +42,7 @@ int main(int argc, char* argv[])  {
         exit(1);
     }
 
+    // Load inputImage into memory
     int width, height, channels;
     unsigned char* inImage = stbi_load(inputImage, &width, &height, &channels, 0);
     
@@ -54,18 +56,22 @@ int main(int argc, char* argv[])  {
     printf("           Height : %d px\n", height);
     printf("         Channels : %d\n\n", channels);
 
+    // Create blockified resolution
     int blockHeight = 194;  // Max block height (256) - sea level (62)
     int blockWidth = round(width * ((float)blockHeight / height));  // Keeps ratio
     printf("INFO: Blockified resolution!\n");
     printf("      Block Width : %d blocks\n", blockWidth);
     printf("     Block Height : %d blocks\n\n", blockHeight);
 
+    // Allocate output image variables memory
     char* outImage = (unsigned char *) malloc(blockWidth * blockHeight * channels);
 
+    // Resize input image to blockified resolution
     stbir_resize_uint8(inImage , width , height , 0,
                        outImage, blockWidth, blockHeight, 0, channels);
     printf("INFO: Resized image!\n\n");
 
+    // Save modified image
     stbi_write_png("blockifiy.png", blockWidth, blockHeight, channels, outImage, blockWidth * channels);    
     printf("INFO: Saved resized image!\n\n");              
 
